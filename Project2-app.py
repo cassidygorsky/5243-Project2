6e0386ca73a6d3cdb4fc44b264e6335252118973
Cassidy Gorsky <cg3520@columbia.edu>
2025-03-12 19:49:11 -0400

    Refactor reactive data handling and improve dataset reset logic in Shiny app

--- Project2-app.py
+++ Project2-app.py
@@ -482,16 +482,20 @@ def server(input, output, session):
         
     # Create a reactive data store to hold the dataset
     stored_data = reactive.Value(None)  # Starts as None, will be set by get_data()
+    trigger = reactive.Value(0)  # Separate trigger for reset_data()
 
     # Reset store_data when a new dataset is selected
+    #@reactive.event(input.data_source)
     @reactive.effect #run whenever it's dependencies change
     def reset_data():
-        """When the user selects a new dataset, reset the stored dataset."""
-        stored_data.set(get_data())  # Set store_data to the freshly loaded dataset
+        """When the user selects a new dataset, reset the stored dataset.""" 
+        #trigger.get() # Depend on trigger to reset stored_data
+        stored_data.set(get_data())  # Set store_data to the freshly loaded dataset. depend on get_data()
 
     # Reactive function to read uploaded file
     @reactive.calc
     def get_data(): #to do: need to create a button to call this on each page
+        #trigger.set(trigger.get() + 1)
         if input.data_source() == "Use Default Data":
             return default_data.copy()
 
@@ -526,17 +530,16 @@ def server(input, output, session):
     @output
     @render.table
     def table(): #refresh data
-        return stored_data
+        return stored_data.get()
     
     ## TEST SAVE BUTTON. To delete
     #save updated data after cleaning
     @reactive.event(input.save_changes_cleaning)
     def modify_data_cleaning():
         """Modify data when 'Save Changes' is clicked in Tab 1."""
-        df = stored_data
+        df = stored_data.get()
         df["new_column"] = 2  # Example modification
-        #stored_data.set(df)
-        stored_data = df.copy()
+        stored_data.set(df)
 
     @reactive.effect
     def update_column_choices():
